90472e7232416c0025b6a799e9038a07b6adba30
Vladimir Grichina <vgrichina@gmail.com>
2022-02-20 16:58:00 -0700

    Make adjustment to token display

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -21,16 +21,59 @@ export function renderNFT(accountId: string): string {
       </defs>
       <rect x="0" y="0" rx="15" ry="15" width="100%" height="100%" fill="url(#RadialGradient2)">
       </rect>
-      <text x="50%" y="96" style="font-family: sans-serif; font-size: 24px; fill: white;" text-anchor="middle" >Claim at ${accountId}.page</text>
-      <text x="50%" y="464" style="font-family: sans-serif; font-size: 48px; fill: white;" text-anchor="middle" >${accountId}</text>
+      <text x="50%" y="48" style="font-family: sans-serif; font-size: 16px; fill: white;" text-anchor="middle" >Claim at ${accountId}.page</text>
+      <text x="50%" y="464" style="font-family: sans-serif; font-size: 24px; fill: white;" text-anchor="middle" >${accountId}</text>
+
+      <text x="50%" y="352" style="font-family: sans-serif; font-size: 24px; fill: white;" text-anchor="middle" >for sale</text>
+
+      <!-- NOTE: Taken from https://www.svgrepo.com/svg/12039/for-sale -->
+      <svg x="37%" y="37%" width="25%" height="25%"
+        viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512; fill: white;"
+        xml:space="preserve">
+      <g>
+        <g>
+          <g>
+            <path d="M213.333,119.467H384c4.719,0,8.533-3.823,8.533-8.533S388.719,102.4,384,102.4H213.333
+              c-4.719,0-8.533,3.823-8.533,8.533S208.614,119.467,213.333,119.467z"/>
+            <path d="M324.267,230.4c0,4.71,3.814,8.533,8.533,8.533h85.333c4.719,0,8.533-3.823,8.533-8.533s-3.814-8.533-8.533-8.533H332.8
+              C328.081,221.867,324.267,225.69,324.267,230.4z"/>
+            <path d="M332.8,187.733H384c4.719,0,8.533-3.823,8.533-8.533s-3.814-8.533-8.533-8.533h-51.2c-4.719,0-8.533,3.823-8.533,8.533
+              S328.081,187.733,332.8,187.733z"/>
+            <path d="M401.067,273.067H332.8c-4.719,0-8.533,3.823-8.533,8.533s3.814,8.533,8.533,8.533h68.267
+              c4.719,0,8.533-3.823,8.533-8.533S405.786,273.067,401.067,273.067z"/>
+            <path d="M264.533,494.933H179.2c-4.719,0-8.533,3.823-8.533,8.533S174.481,512,179.2,512h85.333c4.719,0,8.533-3.823,8.533-8.533
+              S269.252,494.933,264.533,494.933z"/>
+            <path d="M332.8,494.933h-17.067c-4.719,0-8.533,3.823-8.533,8.533s3.814,8.533,8.533,8.533H332.8
+              c4.719,0,8.533-3.823,8.533-8.533S337.519,494.933,332.8,494.933z"/>
+            <path d="M185.233,227.9l2.5-2.5V281.6c0,4.71,3.814,8.533,8.533,8.533H281.6c4.719,0,8.533-3.823,8.533-8.533v-56.201l2.5,2.5
+              c1.664,1.664,3.849,2.5,6.033,2.5c2.185,0,4.369-0.836,6.033-2.5c3.336-3.337,3.336-8.73,0-12.066L244.966,156.1
+              c-3.337-3.337-8.73-3.337-12.066,0l-59.733,59.733c-3.337,3.337-3.337,8.73,0,12.066
+              C176.503,231.236,181.897,231.236,185.233,227.9z M238.933,174.199l35.209,35.209c-0.623,1.195-1.075,2.483-1.075,3.925v59.733
+              h-25.6v-25.6c0-4.71-3.814-8.533-8.533-8.533s-8.533,3.823-8.533,8.533v25.6h-25.6v-59.733c0-1.442-0.452-2.731-1.075-3.925
+              L238.933,174.199z"/>
+            <path d="M503.467,494.933H366.933c-4.719,0-8.533,3.823-8.533,8.533s3.814,8.533,8.533,8.533h136.533
+              c4.719,0,8.533-3.823,8.533-8.533S508.186,494.933,503.467,494.933z"/>
+            <path d="M162.133,102.4c-23.526,0-42.667,19.14-42.667,42.667v170.667c0,23.526,19.14,42.667,42.667,42.667H435.2
+              c23.526,0,42.667-19.14,42.667-42.667V145.067c0-23.526-19.14-42.667-42.667-42.667h-8.533V68.267h76.8
+              c4.719,0,8.533-3.823,8.533-8.533s-3.814-8.533-8.533-8.533H85.333V25.6c0-14.114-11.486-25.6-25.6-25.6s-25.6,11.486-25.6,25.6
+              v469.333h-25.6c-4.719,0-8.533,3.823-8.533,8.533S3.814,512,8.533,512H128c4.719,0,8.533-3.823,8.533-8.533
+              s-3.814-8.533-8.533-8.533H85.333V93.867c0-4.71-3.814-8.533-8.533-8.533s-8.533,3.823-8.533,8.533v401.067H51.2V25.6
+              c0-4.702,3.823-8.533,8.533-8.533s8.533,3.831,8.533,8.533v34.133c0,4.71,3.814,8.533,8.533,8.533h93.867V102.4H162.133z
+              M179.2,119.467c4.719,0,8.533-3.823,8.533-8.533V68.267H409.6v42.667c0,4.71,3.814,8.533,8.533,8.533H435.2
+              c14.114,0,25.6,11.486,25.6,25.6v170.667c0,14.114-11.486,25.6-25.6,25.6H162.133c-14.114,0-25.6-11.486-25.6-25.6V145.067
+              c0-14.114,11.486-25.6,25.6-25.6H179.2z"/>
+          </g>
+        </g>
+      </g>
+      </svg>
     </svg>
   `;
   return svg;
 }
 
 const NFT_SPEC = 'nft-1.0.0'
-const NFT_NAME = '.near club'
-const NFT_SYMBOL = 'dotNEAR'
+const NFT_SYMBOL = '.near'
 
 @nearBindgen
 class TokenMetadata {
@@ -54,7 +97,7 @@ class TokenMetadata {
 class NFTContractMetadata {
   constructor(
     public spec: string = NFT_SPEC,
-    public name: string = NFT_NAME,
+    public name: string = `${Context.contractName}`,
     public symbol: string = NFT_SYMBOL,
     public icon: string = '',
     // public base_uri: string = '',
@@ -142,7 +185,7 @@ export function nft_tokens_for_owner(
   from_index: u64 = 0,
   limit: u8 = 0
 ): Token[] {
-  const token = nft_token(Context.contractName);  
+  const token = nft_token(Context.contractName);
   if (nft_supply_for_owner(account_id) == 0) {
     return [];
   }
@@ -199,7 +242,7 @@ export function nft_transfer(
   // memo: string | null
 ): void {
   const senderId = Context.predecessor;
-  
+
   assert(senderId != receiver_id, 'The token owner and the receiver should be different');
 
   const stateKey = `state:${token_id}`;
